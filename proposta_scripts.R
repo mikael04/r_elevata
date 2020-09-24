076c4684d16743198a94de8eb16531406ce7e96e
mikael04 <mikael.coletto.eng@gmail.com>
2020-09-24 12:18:28 -0300

    Fiz umas alterações (que não deram certo, terei que ver como fazer), pra mostrar uma empresa diferente, no geral tudo ok, só precisa remover um gráfico (q a outra não tem) e ajustar pra fazer isso automaticamente, coloquei os gráficos de ticket médio (corrigidos, espero q a última vez) também, um pra novos e outro pra usados, e adicionei o gráfico do modo e da forma de pagamento (removi um de distribuição do faturamento por status)

--- proposta_scripts.R
+++ proposta_scripts.R
@@ -22,13 +22,14 @@ library(scales)
 
 
 ####Variavel de teste para não remover e imprimir valores de teste, 1 para teste, 0 para não estou testando, rodando
-teste = F
+teste = T
 
 ####Variável usada para não apagar coisas na dash
 dash = F
 
-####Variável global da empresa a ser trabalhada
-empresa = 16
+##Variável "Global"
+empresa = 16 #Super
+#empresa = 78 #Komatsu
 
 #formatar dinheiro
 func_fmt_din <- function(inteiro)
@@ -194,7 +195,7 @@ if(teste == F){
 }
 
 ##############################################
-###Ticket médio por proposta
+###Ticket médio por proposta (apenas novos)
 
 negocio_produto <- tbl(con, "negocio_produto") %>%
   select(np_id, np_negocio_id, np_produto_id, np_quantidade,np_ativo, np_valor) %>%
@@ -212,9 +213,16 @@ proposta_produto <- tbl(con, "proposta_produto") %>%
 
 p_ij_n_ij_v_ij_pp <- inner_join(prop_ij_neg_ij_vend, proposta_produto, by = c("proposta_id" = "pp_proposta_id"))
 
-##filtro da empresa e também propostas finalizadas
-p_ij_n_ij_pp_empresa <- p_ij_n_ij_v_ij_pp %>%
-  filter(vendedor_empresa_id == empresa, proposta_status == 4)
+##Vou puxar negócio novamente pra pegar a coluna de se é usado ou não (acho que é mais eficiente do que usar a tabela completa desde o início)
+negocio <- tbl(con, "negocio") %>%
+  select(negocio_id, negocio_tipo_negocio) %>%
+  collect()
+
+p_ij_n_ij_v_ij_pp_n <- inner_join(p_ij_n_ij_v_ij_pp, negocio, by = c("proposta_negocio_id" = "negocio_id"))
+
+##filtro da empresa e também propostas finalizadas, também filtrando só negócios novos e com valores acima de 10
+p_ij_n_ij_pp_empresa <- p_ij_n_ij_v_ij_pp_n %>%
+  filter(vendedor_empresa_id == empresa, proposta_status == 4, negocio_tipo_negocio == 'N', pp_valor > 10)
 
 p_ij_n_ij_pp_empresa <- p_ij_n_ij_pp_empresa %>%
   group_by(proposta_id) %>%
@@ -223,7 +231,8 @@ p_ij_n_ij_pp_empresa <- p_ij_n_ij_pp_empresa %>%
 
 ##media geral da empresa
 total_empresa <- sum(p_ij_n_ij_pp_empresa$pp_valor)
-media_empresa <- round(total_empresa/nrow(p_ij_n_ij_pp_empresa), 2)
+n_empresa <- nrow(p_ij_n_ij_pp_empresa)
+media_empresa <- round((total_empresa/n_empresa), 2)
 
 
 ##Calculando ticket médio por categoria
@@ -233,8 +242,8 @@ p_ij_n_ij_pp_ij_prod <- inner_join (p_ij_n_ij_pp_empresa, produto, by= c('pp_pro
   filter(proposta_status == 4, vendedor_empresa_id == empresa) #Proposta finalizada
 
 
-## Primeira vez pra verificar quais são os top10
-pr_top10_fat <- p_ij_n_ij_pp_ij_prod %>%
+## Primeira vez pra verificar quais são os top5
+pr_top5_fat <- p_ij_n_ij_pp_ij_prod %>%
   select(produto_categoria_id, pp_valor) %>%
   group_by(produto_categoria_id) %>%
   mutate(fat = sum(pp_valor)) %>%
@@ -242,42 +251,47 @@ pr_top10_fat <- p_ij_n_ij_pp_ij_prod %>%
   distinct (produto_categoria_id, .keep_all = TRUE) %>%
   collect ()
 
-
-##Isso aqui tudo é pra pegar o top 8 (antes top10, mas aparecia uma categoria que não queríamos), substituir os que não estão por -Outros e depois refazer a média
-top10_fat <- head.matrix(pr_top10_fat, n=10)
+##Isso aqui tudo é pra pegar o top 5 (antes top10, mas aparecia uma categoria que não queríamos), substituir os que não estão por -Outros e depois refazer a média
+top5_fat <- head.matrix(pr_top5_fat, n=5)
 
 ##Vou fazer um join pra pegar os nomes de cada categoria
 categoria <- tbl(con, "categoria") %>%
   select(categoria_id, categoria_nome) %>%
   collect()
 
-top10_fat_ij_cat <- inner_join(top10_fat, categoria, by = c("produto_categoria_id"="categoria_id"))
-top10_fat_ij_cat <- top10_fat_ij_cat[, -1]
-top10_fat_ij_cat <- top10_fat_ij_cat[, -2:-4]
-top10_fat_ij_cat <- as.data.frame(top10_fat_ij_cat)
+##Super apenas
+categoria$categoria_nome[categoria$categoria_id == '120160830102326'] <- 'SEMEADORA'
+#Arrumando encoding
+Encoding(categoria$categoria_nome) <- 'latin1'
+
+top5_fat_ij_cat <- inner_join(top5_fat, categoria, by = c("produto_categoria_id"="categoria_id"))
+top5_fat_ij_cat <- top5_fat_ij_cat[, -1]
+top5_fat_ij_cat <- top5_fat_ij_cat[, -2:-4]
+top5_fat_ij_cat <- as.data.frame(top5_fat_ij_cat)
 
 ##Aqui estou juntando para agrupar primeiro através do na
-pr_top10_fat_aux <- left_join(pr_top10_fat, top10_fat_ij_cat, by=c("produto_categoria_id" = "produto_categoria_id"))
-pr_top10_fat_aux$produto_categoria_id[is.na(pr_top10_fat_aux$categoria_nome)] <- "-1"
+pr_top5_fat_aux <- left_join(pr_top5_fat, top5_fat_ij_cat, by=c("produto_categoria_id" = "produto_categoria_id"))
+pr_top5_fat_aux$produto_categoria_id[is.na(pr_top5_fat_aux$categoria_nome)] <- "-1"
+pr_top5_fat_aux$categoria_nome[is.na(pr_top5_fat_aux$categoria_nome)] <- "OUTRAS *"
 
 
-##Depois de setar as linhas q tem nome de coluna = na (não estão no top10) e ter setado todas pra id = -1, faço a soma dos n e faturamentos
-n_out <- sum(pr_top10_fat_aux$n[which(pr_top10_fat_aux$produto_categoria_id=="-1")])
-fat_out <- sum(pr_top10_fat_aux$fat[which(pr_top10_fat_aux$produto_categoria_id=="-1")])
+##Depois de setar as linhas q tem nome de coluna = na (não estão no top5) e ter setado todas pra id = -1, faço a soma dos n e faturamentos
+n_out <- sum(pr_top5_fat_aux$n[which(pr_top5_fat_aux$produto_categoria_id=="-1")])
+fat_out <- sum(pr_top5_fat_aux$fat[which(pr_top5_fat_aux$produto_categoria_id=="-1")])
 
-##E então, repasso o valor de volta para a coluna "outros", de faturamento total (de todas as categorias menos as q estão no top10) e número de vezes que aparecem
-pr_top10_fat_aux$n[pr_top10_fat_aux$produto_categoria_id=='-1'] <- n_out
-pr_top10_fat_aux$fat[pr_top10_fat_aux$produto_categoria_id=='-1'] <- fat_out
+##E então, repasso o valor de volta para a coluna "outros", de faturamento total (de todas as categorias menos as q estão no top5) e número de vezes que aparecem
+pr_top5_fat_aux$n[pr_top5_fat_aux$produto_categoria_id=='-1'] <- n_out
+pr_top5_fat_aux$fat[pr_top5_fat_aux$produto_categoria_id=='-1'] <- fat_out
 
 ##E aqui removo as demais linhas, que já foram adicionadas a "Outros"
-pr_top10_fat_aux <- pr_top10_fat_aux[!is.na(pr_top10_fat_aux$categoria_nome),]
+pr_top5_fat_aux <- pr_top5_fat_aux[!is.na(pr_top5_fat_aux$categoria_nome),]
 
 ##Aqui só renomeio pra ver que a categoria outros tem um * representando a soma de todas as outras categorias também
-pr_top10_fat_aux$categoria_nome[(pr_top10_fat_aux$categoria_nome == "OUTRA")] <- "OUTRA *"
+#pr_top5_fat_aux$categoria_nome[(pr_top5_fat_aux$categoria_nome == "OUTRA")] <- "OUTRA *" #Não mais necessário pq já tneho que adicionar a categoria outra então já faço com nome que quero
 
 
 ## Aqui já estõu fazendo a média das categorias
-pr_top10_fat_med <- pr_top10_fat_aux %>%
+pr_top5_fat_med <- pr_top5_fat_aux %>%
   select(categoria_nome, produto_categoria_id, fat, n) %>%
   group_by(produto_categoria_id) %>%
   mutate(fat_med = fat/n) %>%
@@ -285,7 +299,8 @@ pr_top10_fat_med <- pr_top10_fat_aux %>%
   collect ()
 
 
-fat_tot_categorias <- pr_top10_fat_med
+fat_tot_categorias <- pr_top5_fat_med
+
 ##Adicionando a matriz pra poder exibir no gráfico
 fat_tot_categorias <- fat_tot_categorias %>%
   mutate(med_emp = media_empresa) %>%
@@ -297,7 +312,7 @@ ax <- list(
   title = "",
   showticklabels = TRUE)
 p3 <- plot_ly(fat_tot_categorias, type = "bar", x = ~categoria_nome, y = ~fat_med,
-              name = 'Ticket médio por categoria',
+              name = 'Categorias',
               marker = list(color = 'lightblue'),
               text = ~paste(categoria_nome,'<br>' , func_fmt_din(fat_med)),
               hoverinfo = "text")
@@ -306,7 +321,7 @@ p3 <- p3 %>%
   layout(barmode = 'identity', xaxis = ax, yaxis = ax)
 
 p3 <- p3 %>% add_trace(type = 'scatter', mode = 'markers+line', yaxis = 'y2',
-                       name = 'Ticket médio da empresa',
+                       name = 'Empresa (geral)',
                        x = ~categoria_nome,
                        y = ~med_emp,
                        line = list(color = 'red'),
@@ -316,22 +331,155 @@ p3 <- p3 %>% add_trace(type = 'scatter', mode = 'markers+line', yaxis = 'y2',
 
 p3 <- p3 %>%
   layout(
-    yaxis = list(side = 'left', title = 'Faturamento', showgrid = TRUE, zeroline = FALSE, title = ''),
+    autosize = F,
+    yaxis = list(side = 'left', title = '', showgrid = TRUE, zeroline = FALSE, title = '', range = c(0,2000000)),
     #range nos dois eixos iguais pra ficar na mesma proporção
-    yaxis2 = list(overlaying = "y", showgrid = FALSE, zeroline = FALSE, showticklabels= F, range = c(0,400000)), yaxis = list(range = c(0,400000)),
+    yaxis2 = list(overlaying = "y", showgrid = FALSE, zeroline = FALSE, showticklabels= F, range = c(0,2000000)),
     ##aqui eu ajusto onde quero que apareça a legenda
-    legend = list(x=0.8, y=0.9)#)
+    legend = list(x=0.7, y=0.8)#)
   )
 if(dash == F){
   p3
 }
 
+###Ticket médio por proposta (apenas usados)
+
+p_ij_n_ij_v_ij_pp_n <- inner_join(p_ij_n_ij_v_ij_pp, negocio, by = c("proposta_negocio_id" = "negocio_id"))
+
+##filtro da empresa e também propostas finalizadas, também filtrando só negócios novos e com valores acima de 10
+p_ij_n_ij_pp_empresa_us <- p_ij_n_ij_v_ij_pp_n %>%
+  filter(vendedor_empresa_id == empresa, proposta_status == 4, negocio_tipo_negocio == 'U', pp_valor > 10)
+
+
+p_ij_n_ij_pp_empresa_us <- p_ij_n_ij_pp_empresa_us %>%
+  group_by(proposta_id) %>%
+  mutate (valor_proposta = sum(pp_valor)) %>%
+  collect ()
+
+##media geral da empresa
+total_empresa_us <- sum(p_ij_n_ij_pp_empresa_us$pp_valor)
+n_empresa_us <- nrow(p_ij_n_ij_pp_empresa_us)
+media_empresa_us <- round((total_empresa_us/n_empresa_us), 2)
+
+
+##Calculando ticket médio por categoria
+
+p_ij_n_ij_pp_ij_prod_us <- inner_join (p_ij_n_ij_pp_empresa_us, produto, by= c('pp_produto_id' = 'produto_id')) %>%
+  select (proposta_id, proposta_negocio_id, proposta_data_cadastro, proposta_status, pp_id, pp_produto_id, pp_quantidade, pp_valor, pp_ativo, produto_categoria_id, vendedor_empresa_id, negocio_tipo_negocio) %>%
+  filter(proposta_status == 4, vendedor_empresa_id == empresa) #Proposta finalizada
+
+##Teste
+##Adicionar categoria pra ver se é isso mesmo
+p_ij_n_ij_pp_ij_prod_us <- inner_join(p_ij_n_ij_pp_ij_prod_us, categoria, by = c("produto_categoria_id" = "categoria_id"))
+
+##Fim do teste
+
+## Primeira vez pra verificar quais são os top5
+pr_top5_fat_us <- p_ij_n_ij_pp_ij_prod_us %>%
+  select(produto_categoria_id, pp_valor) %>%
+  group_by(produto_categoria_id) %>%
+  mutate(fat = sum(pp_valor)) %>%
+  mutate(n = n()) %>%
+  distinct (produto_categoria_id, .keep_all = TRUE) %>%
+  collect ()
+
+##Isso aqui tudo é pra pegar o top 5 (antes top10, mas aparecia uma categoria que não queríamos), substituir os que não estão por -Outros e depois refazer a média
+top5_fat_us <- head.matrix(pr_top5_fat_us, n=5)
+
+##Vou fazer um join pra pegar os nomes de cada categoria
+categoria <- tbl(con, "categoria") %>%
+  select(categoria_id, categoria_nome) %>%
+  collect()
+
+#Arrumando encoding
+Encoding(categoria$categoria_nome) <- 'latin1'
+
+top5_fat_ij_cat_us <- inner_join(top5_fat_us, categoria, by = c("produto_categoria_id"="categoria_id"))
+top5_fat_ij_cat_us <- top5_fat_ij_cat_us[, -1]
+top5_fat_ij_cat_us <- top5_fat_ij_cat_us[, -2:-4]
+top5_fat_ij_cat_us <- as.data.frame(top5_fat_ij_cat_us)
+
+##Aqui estou juntando para agrupar primeiro através do na
+pr_top5_fat_aux <- left_join(pr_top5_fat_us, top5_fat_ij_cat, by=c("produto_categoria_id" = "produto_categoria_id"))
+pr_top5_fat_aux$produto_categoria_id[is.na(pr_top5_fat_aux$categoria_nome)] <- "-1"
+pr_top5_fat_aux$categoria_nome[is.na(pr_top5_fat_aux$categoria_nome)] <- "OUTRAS *"
+
+
+##Depois de setar as linhas q tem nome de coluna = na (não estão no top5) e ter setado todas pra id = -1, faço a soma dos n e faturamentos
+n_out <- sum(pr_top5_fat_aux$n[which(pr_top5_fat_aux$produto_categoria_id=="-1")])
+fat_out <- sum(pr_top5_fat_aux$fat[which(pr_top5_fat_aux$produto_categoria_id=="-1")])
+
+##E então, repasso o valor de volta para a coluna "outros", de faturamento total (de todas as categorias menos as q estão no top5) e número de vezes que aparecem
+pr_top5_fat_aux$n[pr_top5_fat_aux$produto_categoria_id=='-1'] <- n_out
+pr_top5_fat_aux$fat[pr_top5_fat_aux$produto_categoria_id=='-1'] <- fat_out
+
+##E aqui removo as demais linhas, que já foram adicionadas a "Outros"
+pr_top5_fat_aux <- pr_top5_fat_aux[!is.na(pr_top5_fat_aux$categoria_nome),]
+
+##Aqui só renomeio pra ver que a categoria outros tem um * representando a soma de todas as outras categorias também
+#pr_top5_fat_aux$categoria_nome[(pr_top5_fat_aux$categoria_nome == "OUTRA")] <- "OUTRA *" #Não mais necessário pq já tneho que adicionar a categoria outra então já faço com nome que quero
+
+
+## Aqui já estõu fazendo a média das categorias
+pr_top5_fat_med_us <- pr_top5_fat_aux %>%
+  select(categoria_nome, produto_categoria_id, fat, n) %>%
+  group_by(produto_categoria_id) %>%
+  mutate(fat_med = fat/n) %>%
+  distinct (produto_categoria_id, .keep_all = TRUE) %>%
+  collect ()
+
+
+fat_tot_categorias_us <- pr_top5_fat_med_us
+
+
+##Adicionando a matriz pra poder exibir no gráfico
+fat_tot_categorias_us <- fat_tot_categorias_us %>%
+  mutate(med_emp = media_empresa_us) %>%
+  collect()
+
+## Gráfico 6.5 - Ticket médio de usados por categoria e geral da empresa
+ax <- list(
+  autotick = TRUE,
+  title = "",
+  showticklabels = TRUE)
+p4 <- plot_ly(fat_tot_categorias_us, type = "bar", x = ~categoria_nome, y = ~fat_med,
+              name = 'Categorias',
+              marker = list(color = 'lightblue'),
+              text = ~paste(categoria_nome,'<br>' , func_fmt_din(fat_med)),
+              hoverinfo = "text")
+
+p4 <- p4 %>%
+  layout(barmode = 'identity', xaxis = ax, yaxis = ax)
+
+p4 <- p4 %>% add_trace(type = 'scatter', mode = 'markers+line', yaxis = 'y2',
+                       name = 'Empresa (geral)',
+                       x = ~categoria_nome,
+                       y = ~med_emp,
+                       line = list(color = 'red'),
+                       text = ~paste('Ticket médio<br>' , func_fmt_din(med_emp)),
+                       hoverinfo = "text",
+                       marker = list(color = 'orange'))
+
+p4 <- p4 %>%
+  layout(
+    autosize = F,
+    yaxis = list(side = 'left', title = '', showgrid = TRUE, zeroline = FALSE, title = '', range = c(0,170000)),
+    #range nos dois eixos iguais pra ficar na mesma proporção
+    yaxis2 = list(overlaying = "y", showgrid = FALSE, zeroline = FALSE, showticklabels= F, range = c(0,170000)),
+    ##aqui eu ajusto onde quero que apareça a legenda
+    legend = list(x=0.7, y=0.8)#)
+  )
+if(dash == F){
+  p4
+}
+
+
 
 if(teste == F){
   #tabelas
-  rm(ax, categoria, fat_tot_categorias, p_ij_n_ij_pp_empresa, pr_top10_fat, pr_top10_fat_aux,
-     pr_top10_fat_med, produto, proposta_produto, top10_fat, top10_fat_ij_cat, p_ij_n_ij_pp_ij_prod,
-     p_ij_n_ij_v_ij_pp, negocio_produto)
+  rm(ax, categoria, fat_tot_categorias, p_ij_n_ij_pp_empresa, p_ij_n_ij_pp_empresa_us, pr_top5_fat, pr_top5_fat_aux,
+     pr_top5_fat_med, produto, proposta_produto, top5_fat, top5_fat_ij_cat, p_ij_n_ij_pp_ij_prod,
+     p_ij_n_ij_v_ij_pp, p_ij_n_ij_v_ij_pp_n, negocio_produto, negocio)
   #variáveis
   rm(fat_out, media_empresa, n_out, total_empresa)
 }
@@ -379,17 +527,17 @@ ax <- list(
   autotick = TRUE,
   title = "",
   showticklabels = TRUE)
-p4 <- plot_ly(p_ij_n_ij_pp_sum_cat, x = ~proposta_status, y = ~valor_status, type = 'bar',
+p5 <- plot_ly(p_ij_n_ij_pp_sum_cat, x = ~proposta_status, y = ~valor_status, type = 'bar',
               name = 'Faturamento por status',
               marker = list(color = c("#ADD8E6", "#00BFFF", "orange", "#DE0D26", "#32CD32")),
               text = ~paste(proposta_status,'<br>' , func_fmt_din(valor_status)),
               hoverinfo = "text")
 
-p4 <- p4 %>%
+p5 <- p5 %>%
   layout(barmode = 'identity', xaxis = ax, yaxis = ax)
 
 if(dash == F){
-  p4
+  p5
 }
 if(teste == F){
   #tabelas
@@ -444,21 +592,21 @@ p_ij_ppa_cont_forma_ij_pmf <- inner_join (p_ij_ppa_count_forma, proposta_modo_fo
 
 
 ##Gráfico 8 - Distribuição das formas de pagamento
-p5 <- plot_ly()
-p5 <- p5 %>%
+p6 <- plot_ly()
+p6 <- p6 %>%
   add_pie(data = p_ij_ppa_cont_modo_ij_pmf, values = ~cont_modo, labels = ~Modo,
           name = 'Modo de pagamento'
   )
 if(dash == F){
-  p5
+  p6
 }
-p6 <- plot_ly()
-p6 <- p6 %>%
+p7 <- plot_ly()
+p7 <- p7 %>%
   add_pie(data = p_ij_ppa_cont_forma_ij_pmf, values = ~cont_forma, labels = ~Forma,
           name = 'Forma de pagamento'
   )
 if(dash == F){
-  p6
+  p7
 }
 
 if(teste == F){
