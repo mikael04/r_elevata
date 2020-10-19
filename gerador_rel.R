a503a70bbcdc7eebd4a6c83619098455175e86d4
mikael04 <mikael.coletto.eng@gmail.com>
2020-10-19 09:40:23 -0300

    Resolvidos os problemas da komatsu, adicionadas as cores para as marcas q faltavam (marcas_icon) Agora terei duas dashs de mapa (dash_visita_mapas e dash_visitas_mapas_k), já que não posso alterar o html

--- gerador_rel.R
+++ gerador_rel.R
@@ -1,13 +1,13 @@
 ##Usado para super e komatsu
 ##renderiza dash_negocios_propostas e dash_visitas
 render_report_neg_prop <- function(var1) {
-  
+
   template <- "dash_negocios_propostas.Rmd"
-  
+
   out_file <- sprintf("Dashs/Negocios_Propostas_%s", var1)
-  
+
   parameters <- list(variable1 = var1)
-  
+
   rmarkdown::render(template,
                     output_file = out_file,
                     params = parameters)
@@ -15,13 +15,39 @@ render_report_neg_prop <- function(var1) {
 }
 
 render_report_vis_map <- function(var1) {
-  
+
   template <- "dash_visitas_mapas.Rmd"
-  
+
   out_file <- sprintf("Dashs/Visitas_Mapas_%s", var1)
-  
+
   parameters <- list(variable1 = var1)
-  
+
+  rmarkdown::render(template,
+                    output_file = out_file,
+                    params = parameters)
+  invisible(TRUE)
+}
+render_report_vis_map_k <- function(var1) {
+
+  template <- "dash_visitas_mapas_k.Rmd"
+
+  out_file <- sprintf("Dashs/Visitas_Mapas_%s", var1)
+
+  parameters <- list(variable1 = var1)
+
+  rmarkdown::render(template,
+                    output_file = out_file,
+                    params = parameters)
+  invisible(TRUE)
+}
+render_report_vis_map_k <- function(var1) {
+
+  template <- "dash_visitas_mapas_k.Rmd"
+
+  out_file <- sprintf("Dashs/Visitas_Mapas_%s", var1)
+
+  parameters <- list(variable1 = var1)
+
   rmarkdown::render(template,
                     output_file = out_file,
                     params = parameters)
@@ -29,26 +55,26 @@ render_report_vis_map <- function(var1) {
 }
 
 render_report_neg_vis <- function(var1) {
-  
+
   template <- "dash_negocios_visitas.Rmd"
-  
+
   out_file <- sprintf("Dashs/Negocios_Visitas_%s", var1)
-  
+
   parameters <- list(variable1 = var1)
-  
+
   rmarkdown::render(template,
                     output_file = out_file,
                     params = parameters)
   invisible(TRUE)
 }
 render_report_map <- function(var1) {
-  
+
   template <- "dash_mapas.Rmd"
-  
+
   out_file <- sprintf("Dashs/Mapas_%s", var1)
-  
+
   parameters <- list(variable1 = var1)
-  
+
   rmarkdown::render(template,
                     output_file = out_file,
                     params = parameters)
@@ -57,11 +83,13 @@ render_report_map <- function(var1) {
 
 library(purrr)
 ##Gerando das que tem propostas (duas dashs separadas, a primeira negócios+propostas, segunda visitas+clientes)
-params_list_1 <- list(list("Super","Komatsu"))
-#params_list_1 <- list(list("Super","Komatsu")) ##testar apenas a visitas_mapa
-
+#params_list_1 <- list(list("Super","Komatsu"))
+params_list_1 <- list(list("Super"),list("Komatsu"))
+##params_list_1 <- list(list("Super","Komatsu")) ##testar apenas a visitas_mapa
 pmap(params_list_1, render_report_neg_prop)
-pmap(params_list_1[[1]][[1]], render_report_vis_map)
+#pmap(params_list_1, render_report_vis_map)
+pmap(params_list_1[[1]], render_report_vis_map)
+pmap(params_list_1[[2]], render_report_vis_map_k)
 
 ##Gerando das que não tem propostas (uma dash, negocios+visitas+clientes)
 params_list_2 <- list(list("Amazonia","Araguaia", "MS", "Simex", "Taisa"))
