38a882bec01ccb62e0991761b35372d54457474c
mikael04 <mikael.coletto.eng@gmail.com>
2020-10-19 09:40:23 -0300

    Resolvidos os problemas da komatsu, adicionadas as cores para as marcas q faltavam (marcas_icon) Agora terei duas dashs de mapa (dash_visita_mapas e dash_visitas_mapas_k), já que não posso alterar o html

--- gerador_rel.R
+++ gerador_rel.R
@@ -27,6 +27,19 @@ render_report_vis_map <- function(var1) {
                     params = parameters)
   invisible(TRUE)
 }
+render_report_vis_map_k <- function(var1) {
+  
+  template <- "dash_visitas_mapas_k.Rmd"
+  
+  out_file <- sprintf("Dashs/Visitas_Mapas_%s", var1)
+  
+  parameters <- list(variable1 = var1)
+  
+  rmarkdown::render(template,
+                    output_file = out_file,
+                    params = parameters)
+  invisible(TRUE)
+}
 
 render_report_neg_vis <- function(var1) {
   
@@ -57,11 +70,13 @@ render_report_map <- function(var1) {
 
 library(purrr)
 ##Gerando das que tem propostas (duas dashs separadas, a primeira negócios+propostas, segunda visitas+clientes)
-params_list_1 <- list(list("Super","Komatsu"))
+#params_list_1 <- list(list("Super","Komatsu"))
+params_list_1 <- list(list("Super"),list("Komatsu"))
 #params_list_1 <- list(list("Super","Komatsu")) ##testar apenas a visitas_mapa
-
 pmap(params_list_1, render_report_neg_prop)
-pmap(params_list_1[[1]][[1]], render_report_vis_map)
+#pmap(params_list_1, render_report_vis_map)
+pmap(params_list_1[[1]], render_report_vis_map)
+pmap(params_list_1[[2]], render_report_vis_map_k)
 
 ##Gerando das que não tem propostas (uma dash, negocios+visitas+clientes)
 params_list_2 <- list(list("Amazonia","Araguaia", "MS", "Simex", "Taisa"))
