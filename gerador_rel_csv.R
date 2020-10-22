cb45bed6c4bb377531d83fd22b7c3f5ffbad55b4
Mikael <mikael.coletto.eng@gmail.com>
2020-10-22 12:17:43 -0300

    Alterando gerador das dashs para cada dash (depois de quebrada)

--- gerador_rel_csv.R
+++ gerador_rel_csv.R
@@ -1,19 +1,26 @@
 library(purrr)
 library(tictoc)
 
-tictoc::tic("")
+#tictoc::tic("")
 setwd("E:\\Mikael\\OneDrive\\Projetos\\Scripts_R\\r_elevata")
-Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc")
+#Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc")
 ##Usado para super e komatsu
 ##renderiza dash_negocios_propostas e dash_visitas_mapas
 
-render_report_neg_prop_csv <- function(var1) {
+# geral
+# marcas
+# marcas_k
+# negocios
+# propostas
+# visitas_clientes
+
+render_report_ger <- function(var1) {
   
-  template <- "dash_negocios_propostas_csv.Rmd"
+  template <- "dash_geral.Rmd"
   
   #Teste (nome da empresa, mais fácil de analisar)
   # out_file <- sprintf("Dashs/Negocios_Propostas_%s, var1)
-  out_file <- sprintf("Dashs/Negocios_Propostas_%s", convert_str_emp(var1))
+  out_file <- sprintf("Dashs/Geral_%s", convert_str_emp(var1))
   
   parameters <- list(variable1 = var1)
   
@@ -23,63 +30,78 @@ render_report_neg_prop_csv <- function(var1) {
   invisible(TRUE)
 }
 
-render_report_vis_map_csv <- function(var1) {
-
-  template <- "dash_visitas_mapas_csv.Rmd"
+render_report_mar <- function(var1) {
   
-  #Teste (nome da empresa, mais fácil de analisar)
-  # out_file <- sprintf("Dashs/Negocios_Visitas_%s", var1)
-  out_file <- sprintf("Dashs/Visitas_Mapas_%s", convert_str_emp(var1))
-
-  parameters <- list(variable1 = var1)
-
-  rmarkdown::render(template,
-                    output_file = out_file,
-                    params = parameters)
-  invisible(TRUE)
-}
-render_report_vis_map_k_csv <- function(var1) {
-
-  template <- "dash_visitas_mapas_k_csv.Rmd"
-  
-  #Teste (nome da empresa, mais fácil de analisar)
-  # out_file <- sprintf("Dashs/Visitas_Mapas_%s", var1)
-  out_file <- sprintf("Dashs/Visitas_Mapas_%s", convert_str_emp(var1))
-
-  parameters <- list(variable1 = var1)
-
-  rmarkdown::render(template,
-                    output_file = out_file,
-                    params = parameters)
-  invisible(TRUE)
-}
-
-render_report_neg_vis_csv <- function(var1) {
-  
-  template <- "dash_negocios_visitas_csv.Rmd"
-  
-  #Teste (nome da empresa, mais fácil de analisar)
-  # out_file <- sprintf("Dashs/Negocios_Visitas_%s", var1)
-  out_file <- sprintf("Dashs/Negocios_Visitas_%s", convert_str_emp(var1))
-  
-  parameters <- list(variable1 = var1)
-  
-  rmarkdown::render(template,
-                    output_file = out_file,
-                    params = parameters)
-  invisible(TRUE)
-}
-
-
-render_report_map_csv <- function(var1) {
-  
-  template <- "dash_mapas_csv.Rmd"
+  template <- "dash_marcas.Rmd"
   #Teste (nome da empresa, mais fácil de analisar)
   # out_file <- sprintf("Dashs/Mapas_%s", var1)
-  out_file <- sprintf("Dashs/Mapas_%s", convert_str_emp(var1))
+  out_file <- sprintf("Dashs/Marcas_%s", convert_str_emp(var1))
   
   parameters <- list(variable1 = var1)
   
+  rmarkdown::render(template,
+                    output_file = out_file,
+                    params = parameters)
+  invisible(TRUE)
+}
+
+render_report_mar_k <- function(var1) {
+  
+  template <- "dash_marcas_k.Rmd"
+  #Teste (nome da empresa, mais fácil de analisar)
+  # out_file <- sprintf("Dashs/Mapas_%s", var1)
+  out_file <- sprintf("Dashs/Marcas_%s", convert_str_emp(var1))
+  
+  parameters <- list(variable1 = var1)
+  
+  rmarkdown::render(template,
+                    output_file = out_file,
+                    params = parameters)
+  invisible(TRUE)
+}
+
+render_report_neg <- function(var1) {
+  
+  template <- "dash_negocios.Rmd"
+  
+  #Teste (nome da empresa, mais fácil de analisar)
+  # out_file <- sprintf("Dashs/Negocios_Propostas_%s, var1)
+  out_file <- sprintf("Dashs/Negocios_%s", convert_str_emp(var1))
+  
+  parameters <- list(variable1 = var1)
+  
+  rmarkdown::render(template,
+                    output_file = out_file,
+                    params = parameters)
+  invisible(TRUE)
+}
+
+render_report_prop <- function(var1) {
+  
+  template <- "dash_propostas.Rmd"
+  
+  #Teste (nome da empresa, mais fácil de analisar)
+  # out_file <- sprintf("Dashs/Negocios_Propostas_%s, var1)
+  out_file <- sprintf("Dashs/Propostas_%s", convert_str_emp(var1))
+  
+  parameters <- list(variable1 = var1)
+  
+  rmarkdown::render(template,
+                    output_file = out_file,
+                    params = parameters)
+  invisible(TRUE)
+}
+
+render_report_vis_cli <- function(var1) {
+
+  template <- "dash_visitas_clientes.Rmd"
+  
+  #Teste (nome da empresa, mais fácil de analisar)
+  # out_file <- sprintf("Dashs/Negocios_Visitas_%s", var1)
+  out_file <- sprintf("Dashs/Visitas_Clientes_%s", convert_str_emp(var1))
+
+  parameters <- list(variable1 = var1)
+
   rmarkdown::render(template,
                     output_file = out_file,
                     params = parameters)
@@ -127,27 +149,45 @@ emp_si = 59 # Simex
 emp_su = 16 # Super
 emp_ta = 60 # Taisa
 
-##Gerando das que tem propostas (duas dashs separadas, a primeira negócios+propostas, segunda visitas+clientes)
-#params_list_1 <- list(list("Super","Komatsu"))
-params_list_1 <- list(list("Super"),list("Komatsu"))
-#params_list_1 <- list(list("Super","Komatsu")) ##testar apenas a visitas_mapa
+##Parâmetros em ordem alfabética, uma única lista, fazendo um por um já que alguns não são feitos para todas 
+params_list <- list(list("Amazonia"),list("Araguaia"),list("Komatsu"),list("MS"),list("Simex"),list("Super"),list("Taisa"))
 
-##negocios_proposta Super e Komatsu
-pmap(params_list_1[[1]], render_report_neg_prop_csv)
-pmap(params_list_1[[2]], render_report_neg_prop_csv)
-
-##visitas_mapas Super e Komatsu
-pmap(params_list_1[[1]], render_report_vis_map_csv)
-pmap(params_list_1[[2]], render_report_vis_map_k_csv)
+#Gerando relatórios geral, todas
+pmap(params_list[[1]], render_report_ger)
+pmap(params_list[[2]], render_report_ger)
+pmap(params_list[[3]], render_report_ger)
+pmap(params_list[[4]], render_report_ger)
+pmap(params_list[[5]], render_report_ger)
+pmap(params_list[[6]], render_report_ger)
+pmap(params_list[[7]], render_report_ger)
+#Gerando relatórios marcas, todas menos komatsu
+pmap(params_list[[1]], render_report_mar)
+pmap(params_list[[2]], render_report_mar)
+pmap(params_list[[4]], render_report_mar)
+pmap(params_list[[5]], render_report_mar)
+pmap(params_list[[6]], render_report_mar)
+pmap(params_list[[7]], render_report_mar)
+#Gerando relatórios marcas da komatsu
+pmap(params_list[[3]], render_report_mar_k) #Kom
+#Gerando relatórios negócios, todas
+pmap(params_list[[1]], render_report_neg)
+pmap(params_list[[2]], render_report_neg)
+pmap(params_list[[3]], render_report_neg)
+pmap(params_list[[4]], render_report_neg)
+pmap(params_list[[5]], render_report_neg)
+pmap(params_list[[6]], render_report_neg)
+pmap(params_list[[7]], render_report_neg)
+##Proposta Super e Komatsu
+pmap(params_list[[3]], render_report_prop) #Kom
+pmap(params_list[[6]], render_report_prop) #Sup
+#Gerando relatórios visitas_clientes, todas
+pmap(params_list[[1]], render_report_vis_cli)
+pmap(params_list[[2]], render_report_vis_cli)
+pmap(params_list[[3]], render_report_vis_cli)
+pmap(params_list[[4]], render_report_vis_cli)
+pmap(params_list[[5]], render_report_vis_cli)
+pmap(params_list[[6]], render_report_vis_cli)
+pmap(params_list[[7]], render_report_vis_cli)
 
 
 #############################################################
-
-##Gerando das que não tem propostas (uma dash, negocios+visitas+clientes)
-params_list_2 <- list(list("Amazonia","Araguaia", "MS", "Simex", "Taisa"))
-
-##negocios_visitas "Amazonia","Araguaia", "MS", "Simex", "Taisa"
-pmap(params_list_2, render_report_neg_vis_csv)
-
-##negocios_visitas "Amazonia","Araguaia", "MS", "Simex", "Taisa"
-pmap(params_list_2, render_report_map_csv)
