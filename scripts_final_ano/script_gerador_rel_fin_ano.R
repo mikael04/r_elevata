d7a18a9c6f13d65813cbf412b8302fc859c27c42
mikael04 <mikael.coletto.eng@gmail.com>
2021-02-26 08:10:35 -0300

    alterando caminho, para ficar padronizado com demais scripts de final de ano

--- scripts_final_ano/script_gerador_rel_fin_ano.R
+++ scripts_final_ano/script_gerador_rel_fin_ano.R
@@ -16,7 +16,7 @@ teste = T
 # visitas_clientes
 
 
-empresas_ativas <- fread("Tabelas_final_ano/empresas_ativas_id.csv") %>%
+empresas_ativas <- fread("scripts_final_ano/Tabelas_final_ano/empresas_ativas_id.csv") %>%
   select(empresa_id)
 
 params_list_i <- empresas_ativas$empresa_id
